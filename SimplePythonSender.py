dea64480b0ab193cb0c10c3f03cba5481720a7b5
LParker885 <67603811+LParker885@users.noreply.github.com>
2021-02-01 15:15:15 -0700

    Update SimplePythonSender.py

--- SimplePythonSender.py
+++ SimplePythonSender.py
@@ -7,6 +7,6 @@ fast = 0
 moveEnabled = 0
 
 while 1:
-  arduino.write(f"a{joints[0]}a{joints[1]}a{joints[2]}a{joints[3]}a{joints[4]}a{moveEnabled}a{fast}a")
-  while ack != "A":
+  arduino.write(f"a{joints[0]}a{joints[1]}a{joints[2]}a{joints[3]}a{joints[4]}a{moveEnabled}a{fast}a")  #this line is the important one, it actually sends the data to the arduino
+  while ack != "A":                                            #don't overload the arduino, wait untill it has sent the ack character to continue
     ack = arduino.readline().decode('utf-8').rstrip()
