f0f7a6b2773d414bf7bb9f19339528a23fde5bf5
LParker885 <67603811+LParker885@users.noreply.github.com>
2021-03-08 11:12:49 -0700

    Update SimplePythonSender.py

--- SimplePythonSender.py
+++ SimplePythonSender.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python3
 import serial
 import time
-arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-arduino.flush()
-joints = [90,90,90,90,90]
-handServos = [90,90,90]
-fast = 0
-moveEnabled = 0
 
-while 1:
-  arduino.write(f"a{joints[0]}a{joints[1]}a{joints[2]}a{joints[3]}a{joints[4]}a{handServos[0]}a{handServos[1]}a{handServos[2]}a{moveEnabled}a{fast}a".encode('utf-8'))  #this line is the important one, it actually sends the data to the arduino
-  while ack != "A":                                            #don't overload the arduino, wait untill it has sent the ack character to continue
-    ack = arduino.readline().decode('utf-8').rstrip()
+data = [90,90,90,90,90,90,90,90,1,0] #j1,j2,j3,j4,j5,h1,h2,h3,moveEnable,fast
+
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+ser.flush()
+
+def changeVals(whichone,val):
+    data[whichone] = val
+
+def sendToArduino():
+    ser.write(f"a{data[0]}a{data[1]}a{data[2]}a{data[3]}a{data[4]}a{data[5]}a{data[6]}a{data[7]}a{data[8]}a{data[9]}a".encode('utf-8'))
+    
+while True:
+    sendToArduino()
+    time.sleep(3)
