c502dc5a2fd0c3222cc8d8277d117db2a8756860
LParker885 <67603811+LParker885@users.noreply.github.com>
2021-03-16 20:28:17 -0600

    Update xyzsendside.py

--- networked-demos/sendside/xyzsendside.py
+++ networked-demos/sendside/xyzsendside.py
@@ -1,6 +1,7 @@
 #sweep test!
 import network
 import time
+import sys
 ip = "169.168.1.101"
 
 network.call(ip)
@@ -8,23 +9,8 @@ network.call(ip)
 
 
 while network.isConnected():
-
-    network.say("8,1")
-    network.say("9,1")
-    num = 2
-    for val in range(90,180,5):
-        out = str(num) + "," + str(val)
-        network.say(out)
-        time.sleep(0.1)
-    for val in range(180,0,-5):
-        out = str(num) + "," + str(val)
-        network.say(out)
-        time.sleep(0.1)
-    for val in range(0,95,5):
-        out = str(num) + "," + str(val)
-        network.say(out)
-        time.sleep(0.1)
-        
-    time.sleep(1)
-    network.say("8,0")
-    time.sleep(5)
+    phrase = input() #phrase should look like 60,12,16,90,180 or theta,x,y,h1,h2 where theta is the angle as measured from the back of the robot (facing forward = 180), x is how far out the hand should be, y is how far up it should be, h1 is hand tilt, h2 is hand rotation
+    print(phrase)
+    network.say(phrase)
+    
+    
