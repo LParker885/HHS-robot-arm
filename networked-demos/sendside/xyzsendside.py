171e967e04351012394551d4af69a23540b9ec8a
LParker885 <67603811+LParker885@users.noreply.github.com>
2021-03-16 20:41:40 -0600

    Update xyzsendside.py

--- networked-demos/sendside/xyzsendside.py
+++ networked-demos/sendside/xyzsendside.py
@@ -9,7 +9,7 @@ network.call(ip)
 
 
 while network.isConnected():
-    phrase = input() #phrase should look like 60,12,16,90,180 or theta,x,y,h1,h2 where theta is the angle as measured from the back of the robot (facing forward = 180), x is how far out the hand should be, y is how far up it should be, h1 is hand tilt, h2 is hand rotation
+    phrase = input() #phrase should look like 60,12,16,90,180 or theta,x,y,h1,h2 where theta is the angle as measured from the back of the robot in degrees (facing forward = 180), x is how far out the hand should be in inches, y is how far up it should be in inches, h1 is hand tilt in degrees 0-180, h2 is hand rotation in degrees 0-360
     print(phrase)
     network.say(phrase)
     
