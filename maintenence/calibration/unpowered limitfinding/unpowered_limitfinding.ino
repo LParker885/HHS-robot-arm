8c3b1903a21357a11197e52b6761cb9de632bcfc
LParker885 <67603811+LParker885@users.noreply.github.com>
2021-09-21 12:31:59 -0600

    Update unpowered_limitfinding.ino

--- maintenence/calibration/unpowered limitfinding/unpowered_limitfinding.ino
+++ maintenence/calibration/unpowered limitfinding/unpowered_limitfinding.ino
@@ -1,1 +1,23 @@
 
+
+
+void setup() {
+Serial.begin(9600);
+ 
+ pinMode(A0,INPUT);
+ pinMode(A1,INPUT);
+ pinMode(A2,INPUT);
+ pinMode(A3,INPUT);  
+ pinMode(A4,INPUT);
+ 
+}
+
+void loop(){
+ for(int i = 0; i < 6; i++){
+  Serial.print(analogRead(i));
+  Serial.print("\t");
+   
+ }
+  Serial.println();
+  
+}
