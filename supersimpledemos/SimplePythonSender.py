7bda42bf4fe00e70626d8d3e2e930b4a0dbb762a
LParker885 <67603811+LParker885@users.noreply.github.com>
2021-10-05 08:23:14 -0600

    Update SimplePythonSender.py

--- supersimpledemos/SimplePythonSender.py
+++ supersimpledemos/SimplePythonSender.py
@@ -2,7 +2,7 @@
 import serial
 import time
 
-data = [90.12,90.12,90.12,90.12,90.12,90.12,90.12,90.12,1,0] #j1,j2,j3,j4,j5,h1,h2,h3,moveEnable,fast
+data = [90.12,90.12,90.12,90.12,90.12,90.12,90.12,90.12,1] #j1,j2,j3,j4,j5,h1,h2,h3,moveEnable
 
 ser = serial.Serial('/dev/ttyACM0', 1000000, timeout=1)
 dataIn = ser.readline().decode('utf-8').rstrip()
@@ -15,5 +15,5 @@ while dataIn != 'A':
 
 while True:
 
-    ser.write(f"{data[0]},{data[1]},{data[2]},{data[3]},{data[4]},{data[5]},{data[6]},{data[7]},{data[8]},{data[9]},".encode('utf-8'))
+    ser.write(f"{data[0]},{data[1]},{data[2]},{data[3]},{data[4]},{data[5]},{data[6]},{data[7]},{data[8]}".encode('utf-8'))
     time.sleep(0.2)
