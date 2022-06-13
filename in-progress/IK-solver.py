4105ecf609b5db160bb00ac0de258baf9700f7ee
LParker885 <67603811+LParker885@users.noreply.github.com>
2022-06-13 15:57:13 -0600

    update iksolver

--- in-progress/IK-solver.py
+++ in-progress/IK-solver.py
@@ -1,8 +1,9 @@
 import math
+from tkinter import Y
 jls = [12,24,18,8]
 
 def solveIK(x,y,z,hand_angle):
-    angs = []
+    angs = [90,90,90,90]
     #X is straight out, Y is up, Z is to the right, hand angle is the requested angle for the wrist from horizontal
     #ignoring the hand for now
     
@@ -14,6 +15,8 @@ def solveIK(x,y,z,hand_angle):
 
     d=math.sqrt(((y-jls[0])**2)+(r**2))
     
+    print((jls[1]**2)+(d**2)-(jls[2]**2)/(2*jls[1]*d))
+    print((y-jls[0])/r)
     angs[1] = 360*((math.acos((jls[1]**2)+(d**2)-(jls[2]**2)/(2*jls[1]*d))+math.atan((y-jls[0])/r))/(2*math.pi))
     angs[2] = 360*(((math.pi/2)+((math.pi)-math.acos((jls[1]**2)+(jls[2]**2)-(d**2)/(2*jls[1]*jls[2]))))/(2*math.pi))
     
@@ -22,3 +25,10 @@ def solveIK(x,y,z,hand_angle):
 
 
     return angs
+
+while 1:
+    x=float(input())
+    y=float(input())
+    z=float(input())
+    h=float(input())
+    print(solveIK(x,y,z,h))
