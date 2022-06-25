7d29609e9fa1776e47c51aa9a05d22fb4d894167
LParker885 <67603811+LParker885@users.noreply.github.com>
2022-06-25 12:59:03 -0600

    update IK for testing tinyIK library

--- in-progress/IK-solver.py
+++ in-progress/IK-solver.py
@@ -1,34 +1,20 @@
-import math
-from tkinter import Y
-jls = [12,24,18,8]
-
-def solveIK(x,y,z,hand_angle):
-    angs = [90,90,90,90]
-    #X is straight out, Y is up, Z is to the right, hand angle is the requested angle for the wrist from horizontal
-    #ignoring the hand for now
-    
-    angs[0] = 360*((math.atan(z/x))/(2*math.pi))
-
-    r=math.sqrt((x**2)+(z**2))
-    y=y-(jls[3]*math.sin(hand_angle))
-    x=x-(jls[3]*math.cos(hand_angle))
-
-    d=math.sqrt(((y-jls[0])**2)+(r**2))
-    
-    print((jls[1]**2)+(d**2)-(jls[2]**2)/(2*jls[1]*d))
-    print((y-jls[0])/r)
-    angs[1] = 360*((math.acos((jls[1]**2)+(d**2)-(jls[2]**2)/(2*jls[1]*d))+math.atan((y-jls[0])/r))/(2*math.pi))
-    angs[2] = 360*(((math.pi/2)+((math.pi)-math.acos((jls[1]**2)+(jls[2]**2)-(d**2)/(2*jls[1]*jls[2]))))/(2*math.pi))
-    
-    angs[3] = (180-angs[1]-(angs[2]-90))+hand_angle
+import tinyik
+import numpy as np
+import time
 
 
+arm = tinyik.Actuator(['y',[.05,.0,.0],'z',[1., .0, .0], 'z',[1., .0, .0], 'z', [.1,.0,.0],'x',[.2,.0,.0]])
 
-    return angs
 
 while 1:
-    x=float(input())
-    y=float(input())
-    z=float(input())
-    h=float(input())
-    print(solveIK(x,y,z,h))
+    try:
+        for x in range(0,10,1):
+            arm.ee=[x/10,1,1]
+            print(arm.ee,np.rad2deg(arm.angles))
+            time.sleep(0.5)
+        for x in range(10,0,-1):
+            arm.ee=[x/10,1,1]
+            print(arm.ee,np.rad2deg(arm.angles))
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        exit()
